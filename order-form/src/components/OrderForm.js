6396560960260cdf65191a8f5ae8b2306173dbd7
Kaela's Workstation <mac3@KaelasWkstation.hiteccopy.local>
2021-10-26 14:44:53 -0500

    starting form

---
+++ order-form/src/components/OrderForm.js
@@ -0,0 +1,10 @@
+
+const OrderForm () =>{
+    return(
+        <div>
+          <p>Choose Your Layout</p>
+          <div><img src ="Assets//WashU/BCOptions.jpg" alt =''/></div>  
+        </div>
+    )
+}
+export default OrderForm;
