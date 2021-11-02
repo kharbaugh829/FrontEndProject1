bae6a420f8dcd5b619ea53edb7562080d242e8dc
Kaela's Workstation <mac3@Kaelas-Workstation.local>
2021-11-02 15:24:19 -0500

    fixed radio formatting hopefully

--- order-form/src/components/OrderForm/PrintOpt.js
+++ order-form/src/components/OrderForm/PrintOpt.js
@@ -5,7 +5,8 @@ const PrintOpt = (props) => {
   const [inkOption, setInkOption] = useState("");
   const [logoType, setLogoType] = useState("");
   const [enteredLogoType, setEnteredLogoType] = "";
-  const quantityChangleHandler = (event) => {
+  const [enteredQuantity, setEnteredQuantity] = "";
+  const quantityChangeHandler = (event) => {
     event.preventDefault();
     let quantity = event.target.value;
     setQuantity(quantity);
@@ -29,83 +30,101 @@ const PrintOpt = (props) => {
     setEnteredLogoType(other);
     props.update(other);
   };
-
-  if (logoType === "Other") {
-    logoType.value = { enteredLogoType };
-  }
+  const enteredQuantityChangeHandler = (event) => {
+    event.preventDefault();
+    let other = event.target.value;
+    setEnteredLogoType(other);
+    props.update(other);
+  };
 
   return (
     <div>
       <h1>Printing Options</h1>
-      <label>
-        Quantity
-        <input
+      <div><label>
+      Quantity
+       <div><input
           type="radio"
-          value="500"
           checked={quantity === "500"}
-          onChange={quantityChangleHandler}
+          onChange={quantityChangeHandler}
+          value="500"
         />
+        <label htmlFor={"500"}>{"500"}</label>
         <input
           type="radio"
           value="1000"
           checked={quantity === "1000"}
-          onChange={quantityChangleHandler}
+          onChange={quantityChangeHandler}
         />
+        <label htmlFor={"1000"}>{"1000"}</label>
         <input
           type="radio"
           value="1500"
           checked={quantity === "1500"}
-          onChange={quantityChangleHandler}
+          onChange={quantityChangeHandler}
         />
-      </label>
-      <label>
-        Ink Options
+        <label htmlFor={"1500"}>{"1500"}</label>
         <input
+          type="radio"
+          value="Other"
+          checked={quantity === "Other"}
+          onChange={quantityChangeHandler}
+        />
+        <label htmlFor={"Other"}>{"Other"}</label>
+        {quantity === "Other" && <input
+          type="text"
+          value=""
+          required = {quantity === "Other"}
+          onChange={enteredQuantityChangeHandler}
+        />}</div> 
+      </label>
+      </div> <label>
+        Ink Options
+       <div> <input
           type="radio"
           value="Color"
           checked={inkOption === "Color"}
           onChange={inkOptionChangeHandler}
-        />
+        /><label htmlFor={"color"}>{"Color Ink"}</label>
         <input
           type="radio"
           value="Black"
           checked={inkOption === "Black"}
           onChange={inkOptionChangeHandler}
-        />
+        /><label htmlFor={"black"}>{"Black Ink"}</label></div> 
       </label>
-      <label>
-        Logo Options
+      <div><label>
+        Logo Options<div>
         <input
           type="radio"
-          value="Chancelor's Logo"
+          value="ChancelorsLogo"
           checked={logoType === "Chancelor's Logo"}
           onChange={logoTypeChangeHandler}
-        />
+        /><label htmlFor={"ChancelorsLogo"}>{"Chancelor's Logo"}</label>
         <input
           type="radio"
           value="Art's & Sciences"
           checked={logoType === "Art's & Sciences"}
           onChange={logoTypeChangeHandler}
-        />
+        /><label htmlFor={"ArtsAndSciences"}>{"Art's & Sciences"}</label>
         <input
           type="radio"
           value="Sam Fox School of Design & Visual Arts"
           checked={logoType === "Sam Fox School of Design & Visual Arts"}
           onChange={logoTypeChangeHandler}
-        />
+        /><label htmlFor={"Sam Fox School of Design & Visual Arts"}>{"Sam Fox School of Design & Visual Arts"}</label>
         <input
           type="radio"
           value="Other"
           checked={logoType === "Other"}
           onChange={logoTypeChangeHandler}
-        />
-        <input
+        /><label htmlFor={"Other"}>{"Other"}</label>
+        {logoType === "Other" && <input
           type="text"
-          value="Other Value"
-          disabled={!logoType === "Other"}
+          value = ''
+          required = {logoType === "Other"}
           onChange={enteredLogoTypeChangeHandler}
-        />
-      </label>
+        />}</div>
+      </label></div>
     </div>
   );
 };
